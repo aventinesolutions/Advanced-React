d8083a4f1bb9fd86a206aede4828855dd4c3700b
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2018-11-15 16:53:25 -0500

    :bear: Hello World simplified as pure function
    Module #1, Step 3 intro to NextJS, Tooling, Routing

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
-
-class Home extends React.Component {
-  render() {
-    return <p>Hello Aventine Solutions!</p>
-  }
-}
+const Home = props => (
+  <div>
+    <p>Hello Aventine Solutions!</p>
+  </div>
+);
 
 export default Home;
