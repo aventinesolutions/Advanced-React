7bc61f15c11c124d87a9af47495c8e7de739be92
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2018-11-15 17:47:34 -0500

    :bear: create a Header component to use in our Page layout
    Module 1, Step 4 custom "_app.js" layout

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Page extends Component {
   render() {
     return (
       <div>
-        <p>(this is the page component)</p>
+        <Header/>
         {this.props.children}
       </div>
     );
