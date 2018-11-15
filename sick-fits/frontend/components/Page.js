9b9669064c6f0d1897b364205467261bc3c2f277
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2018-11-15 17:59:58 -0500

    :bear: create a Meta component for our pages using Next Head
    Module 1, Step 4 custom "_app.js" layout

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Meta from '../components/Meta';
 
 export default class Page extends Component {
   render() {
     return (
       <div>
+        <Meta/>
         <Header/>
         {this.props.children}
       </div>
