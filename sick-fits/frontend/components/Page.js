18964206dd0820d44fd0526294c1b6c94438bb2f
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2018-11-16 15:26:22 -0500

    :bear: remove the Button sample as it is not part of the real app
    Module 2, Step 5 an intro to Styled Components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
-import styled from 'styled-components';
-
-const MyButton = styled.button`
-  background: red;
-  font-size: ${props => (props.huge ? '100px' : '50px')};
-  span {
-    font-size: 100px;
-  }
-`;
 
 export default class Page extends Component {
   render() {
@@ -17,12 +8,6 @@ export default class Page extends Component {
       <div>
         <Meta/>
         <Header/>
-        <MyButton huge>Click
-          <span>here ...</span>
-        </MyButton>
-        <MyButton>Click
-          <span>here ...</span>
-        </MyButton>
         {this.props.children}
       </div>
     );
